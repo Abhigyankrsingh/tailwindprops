d18c982eb2e36ffc9fcc4d23e51b9ad0d836ec9e
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-09 14:15:02 +0530

    Cpde_update

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,26 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-green-400'>Tailwind test</h1>
-      <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://wallpapers.com/images/featured/sukuna-4k-do5f4d61t6h4j945.jpg" alt="" width="384" height="512"/>
-  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sukana
+      <h1 className='p-4 text-black bg-green-400 rounded-xl'>Tailwind test</h1>
+      <div class="relative h-[400px] w-[300px] rounded-md">
+      <img
+        src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+        alt="AirMax Pro"
+        class="z-0 h-full w-full rounded-md object-cover"
+      />
+      <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+      <div class="absolute bottom-4 left-4 text-left">
+        <h1 class="text-lg font-semibold text-white">Delba</h1>
+        <p class="mt-2 text-sm text-gray-300">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
+          debitis?
+        </p>
+        <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+          View Profile →
+        </button>
       </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Software Engineer,Google
-      </div>
-    </figcaption>
-  </div>
-</figure>
+    </div>
+      
     </>
   )
 }
