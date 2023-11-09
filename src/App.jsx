f67e22fbb1c59989a622e774816eaebca010c577
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-09 14:53:26 +0530

    Code_Updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,6 @@ import Card from './components/Card'
 function App() {
   const [count, setCount] = useState(0)
 
-  let myobj = {
-    username: "abhi",
-    age:21
-  }
-
   return (
     <>
       <h1 className='p-4 mb-4 text-black bg-green-400 rounded-xl'>Tailwind test</h1>
