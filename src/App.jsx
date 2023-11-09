b00eb50879e67d1158a81e786de423dc057cc64d
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-09 14:18:15 +0530

    Code_updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1 className='p-4 text-black bg-green-400 rounded-xl'>Tailwind test</h1>
+      <h1 className='p-4 mb-4 text-black bg-green-400 rounded-xl'>Tailwind test</h1>
       <div class="relative h-[400px] w-[300px] rounded-md">
       <img
         src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
