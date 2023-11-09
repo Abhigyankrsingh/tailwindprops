e3d58830e5e1249e55d454c4411335b480896f29
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-09 14:55:19 +0530

    Code_Update

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
     <>
       <h1 className='p-4 mb-4 text-black bg-green-400 rounded-xl'>Tailwind test</h1>
-      <Card username= "Sukana"/>
-      <Card username=" Gojo"/>
+      <Card username= "Sukana" btnText = "click me"/>
+      <Card username=" Gojo" btnText= "visit me"/>
     </>
   )
 }
